209d93e6baa88161571928e2beeaa3e129cc0d3a
al-shep <alecsheppard22@gmail.com>
2021-06-18 17:03:53 -0500

    saving snake

--- asd-projects/snake/2-player/index.js
+++ asd-projects/snake/2-player/index.js
@@ -17,22 +17,30 @@ function runProgram() {
     'UP': [38, 87],
     'DOWN': [40, 83],
     'LEFT': [37, 65],
-    'RIGHT': [39, 68],
+    'RIGHT': [39, 68]
   }
   
   //////////////////////////// Game Item Objects /////////////////////////////////
   
   // snakeArray will contain all the snake-body-objects
-  let snakeArray = []
+  const snakeOne = []
+  const snakeTwo = []
+  const snakeArray = [snakeOne, snakeTwo]
 
   //inital scores
-  let score = 0
-  let applesEaten = []
+  let score = {
+    user1: 0,
+    user2: 0
+  }
+  let applesEaten = {
+    user1: [],
+    user2: []
+  }
 
   let frameCount = 0 // variable to count frames that have been passesd
   
-  //creates the first head and pushes it to snakeArray
-  var head = {
+  //creates the first head for each player and pushes it to their respective snake in snakeArray
+  var head1 = {
     id: nextID('body', snakeArray),
     x: 100,
     y: 200,
@@ -41,7 +49,18 @@ function runProgram() {
     speedX: 0,
     speedY: 0,
   }
-  snakeArray.push(head)
+  snakeArray[0].push(head1)
+
+  var head1 = {
+    id: nextID('body', snakeArray),
+    x: 100,
+    y: 200,
+    width: 20,
+    height: 20,
+    speedX: 0,
+    speedY: 0,
+  }
+  snakeArray[1].push(head2)
 
   //initial coordinates for the apple
   var apple = {
@@ -62,9 +81,11 @@ function runProgram() {
     columns: 22
   }
 
-  //adds 5 body parts to the snake
-  for (var i = 0; i < 5; i++) {
-    AddBody()
+  //adds 5 body parts to the snakes
+  for (var x = 0; x < snakeArray.length; x++) {
+    for (var i = 0; i < 5; i++) {
+      AddBody(x)
+    }
   }
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -107,10 +128,10 @@ function runProgram() {
   }
   
   function moveItems() {
-    // moves the snake's head
+    // moves the snakes' head
     head.x += head.speedX
     head.y += head.speedY
-    // moves the snake's body
+    // moves the snakes' body
     for (var i = snakeLength(); i > 0; i--) {
       snakeArray[i].x = snakeArray[i-1].x
       snakeArray[i].y = snakeArray[i-1].y
@@ -119,10 +140,10 @@ function runProgram() {
   }
 
   function drawItems() { 
-    // draws the snake's head
+    // draws the snakes' head
     $('#head').css('top', head.y)
     $('#head').css('left', head.x)
-    // draws the snake's body
+    // draws the snakes' body
     for (var i = 1; i <= snakeLength(); i++) { 
       $(snakeArray[i].id).css('top', snakeArray[i].y)
       $(snakeArray[i].id).css('left', snakeArray[i].x)
@@ -131,11 +152,11 @@ function runProgram() {
   }
 
   function ateApple() { //what to do if the apple is or isn't eaten
-    if (doCollide(apple, head)) {
-      AddBody()         // adds a body object to snakeArray
+    if (doCollide(apple, head1)) {
+      AddBody(1)         // adds a body object to snakeArray
       updateScore(true) // updates the score and applesEaten count
       newApple()        // randomizes the apple's location
-    } else {
+    } else if (doCollide(apple, head2)) {
       updateScore(false) // doesn't update the score if the apple is eaten
     }
   }
@@ -146,8 +167,8 @@ function runProgram() {
     }
   } 
 
-  function snakeLength() { // shortens calling the largest index of snakeArray
-    return snakeArray.length - 1
+  function snakeLength(user) { // shortens calling the largest index of snakeArray
+    return snakeArray[user].length - 1
   }
 
   function drawApple() {
@@ -157,45 +178,63 @@ function runProgram() {
   }
 
   function keyPress(event, speed) { // translates keyPresses into movement
-    if (event.which === KEY.UP[0] || event.which === KEY.UP[1]) {         //cheking up keys
-      if (head.speedY === 0) {                                            //can't go up if moving down
-        head.speedY = -speed                                              //changes speed to match keyPress
-        head.speedX = 0                                                   //no diagonal movement allowed
+    if (event.which === KEY.UP[0]) {         //cheking up keys
+      if (head1.speedY === 0) {                                            //can't go up if moving down
+        head1.speedY = -speed                                              //changes speed to match keyPress
+        head1.speedX = 0                                                   //no diagonal movement allowed
       }
-    } 
-    else if (event.which === KEY.DOWN[0] || event.which === KEY.DOWN[1]) { //checking down keys
-      if (head.speedY === 0) {                                             //can't go down if moving up
-        head.speedY = speed                                                //changes speed to match keyPress                                                //no diagonal movement allowed
-        head.speedX = 0                                                    //no diagonal movement allowed
+    } else if (event.which === KEY.DOWN[0]) { //checking down keys
+      if (head1.speedY === 0) {                                             //can't go down if moving up
+        head1.speedY = speed                                                //changes speed to match keyPress                                                //no diagonal movement allowed
+        head1.speedX = 0                                                    //no diagonal movement allowed
       }
-    }
-    else if (event.which === KEY.LEFT[0] || event.which === KEY.LEFT[1]) { //checking left keys
-      if (head.speedX === 0){                                              // can't go left if moving right
-        head.speedX = -speed                                               //changes speed to match keyPress
-        head.speedY = 0                                                    //no diagonal movement allowed
+    } else if (event.which === KEY.LEFT[0]) { //checking left keys
+      if (head1.speedX === 0){                                              // can't go left if moving right
+        head1.speedX = -speed                                               //changes speed to match keyPress
+        head1.speedY = 0                                                    //no diagonal movement allowed
       }
-    } 
-    else if (event.which === KEY.RIGHT[0] || event.which === KEY.RIGHT[1]) { //checking right keys
-      if (head.speedX === 0) {                                               // can't go right if moving left
-        head.speedX = speed                                                  //changes speed to match keyPress
-        head.speedY = 0                                                      //no diagonal movement allowed
+    } else if (event.which === KEY.RIGHT[0]) { //checking right keys
+      if (head1.speedX === 0) {                                               // can't go right if moving left
+        head1.speedX = speed                                                  //changes speed to match keyPress
+        head1.speedY = 0                                                      //no diagonal movement allowed
+      }
+    } else if (event.which === KEY.UP[1]) {    //cheking up keys
+      if (head2.speedY === 0) {                                              //can't go up if moving down
+        head2.speedY = -speed                                                //changes speed to match keyPress
+        head2.speedX = 0                                                     //no diagonal movement allowed
+      }
+    } else if (event.which === KEY.DOWN[1]) { //checking down keys
+      if (head2.speedY === 0) {                                             //can't go down if moving up
+        head2.speedY = speed                                                //changes speed to match keyPress                                                //no diagonal movement allowed
+        head2.speedX = 0                                                    //no diagonal movement allowed
+      }
+    } else if (event.which === KEY.LEFT[1]) { //checking left keys
+      if (head2.speedX === 0){                                              // can't go left if moving right
+        head2.speedX = -speed                                               //changes speed to match keyPress
+        head2.speedY = 0                                                    //no diagonal movement allowed
+      }
+    } else if (event.which === KEY.RIGHT[1]) { //checking right keys
+      if (head2.speedX === 0) {                                               // can't go right if moving left
+        head2.speedX = speed                                                  //changes speed to match keyPress
+        head2.speedY = 0                                                      //no diagonal movement allowed
       }
     }
   }
   
-  function AddBody() {
-    let newID = nextID('body', snakeArray) // variable to hold newID
+  function AddBody(user) {
+    let id = 'body' + user
+    let newID = nextID(id, snakeArray[user]) // variable to hold newID
     // creates a new div element for each body part
     $('<div>').appendTo('#board').addClass('gameItem').addClass('body').attr('id', newID)
     return snakeArray.push( { //pushes additional body part
       //creates a new bodyPart
       id: '#' + newID,
-      x: snakeArray[snakeLength()].x,
-      y: snakeArray[snakeLength()].y,
-      width: snakeArray[snakeLength()].width,
-      height: snakeArray[snakeLength()].height,
-      speedX: snakeArray[snakeLength()].speedX,
-      speedY: snakeArray[snakeLength()].speedY,
+      x: snakeArray[user][snakeLength(user)].x,
+      y: snakeArray[user][snakeLength(user)].y,
+      width: snakeArray[user][snakeLength(user)].width,
+      height: snakeArray[user][snakeLength(user)].height,
+      speedX: snakeArray[user][snakeLength(user)].speedX,
+      speedY: snakeArray[user][snakeLength(user)].speedY,
     } )
   }
   
@@ -227,7 +266,7 @@ function runProgram() {
     return base + array.length
   }
   
-  function friendlyWall() {
+  function friendlyWall(head) {
     if (head.x > $board.width) {
       head.x = 0
     } else if (head.x < 0) {
@@ -239,33 +278,61 @@ function runProgram() {
     }
   }
   
-  function updateScore(boolean) {
+  function updateScore(user) {
     //updates sore and apple count
-    if (boolean) {
-      score += 20
-      $('#score').text('Score: ' + score)
-
+    if (user === 1) {
+      //user 1 gets more points
+      score.user1 += 20
+      $('#score').text('Score: ' + score.user1)
       applesEaten.push('one more apple eaten')
-      $('#applesEaten').text('Apples: ' + applesEaten.length)
+      $('#applesEaten').text('Apples: ' + applesEaten.user1.length)
+
+      //user 2 looses points
+      score.user2 -= 5
+      $('#score').text('Score: ' + score.user2)
+      applesEaten.push('one more apple eaten')
+      $('#applesEaten').text('Apples: ' + applesEaten.user2.length)
+    } else if (score === 2) {
+      //user 2 gets more points
+      score.user1 += 20
+      $('#score').text('Score: ' + score.user2)
+      applesEaten.push('one more apple eaten')
+      $('#applesEaten').text('Apples: ' + applesEaten.user2.length)
+
+      //user 1 looses points
+      score.user1 -= 5
+      $('#score').text('Score: ' + score.user1)
+      applesEaten.push('one more apple eaten')
+      $('#applesEaten').text('Apples: ' + applesEaten.user1.length)     
     } else {
       $('#score').text('Score: ' + score)
-      $('#applesEaten').text('Apples: ' + applesEaten.length)
+      $('#applesEaten').text('Apples: ' + applesEaten.user1.length)
+      $('#score').text('Score: ' + score)
+      $('#applesEaten').text('Apples: ' + applesEaten.user2.length)
     }
   }
 
   function deadlyVenom() {
-    for (var i = 2; i <= snakeLength(); i++) {
-      if ((head.x === snakeArray[i].x) && (head.y === snakeArray[i].y) && (head.speedX > 0 || head.speedY > 0) && frameCount > 100) {
-        return true
+    for (var x = 0; x < snakeArray.length; x++) {
+      for (var i = 2; i <= snakeLength(x); i++) {
+        if ((snakeArray[x][0].x === snakeArray[x][i].x) && (snakeArray[x][0].y === snakeArray[x][i].y) && (snakeArray[x][0].speedX > 0 || snakeArray[x][0].speedY > 0) && frameCount > 100) {
+          return true
+        }
       }
+    }
+
+    if (doCollide(head1, head2)) {
+      return true
     }
   }
 
   function newApple() {
     randomApple() // calls a random location for apple to be reset
-    for (var i = 1; i <= snakeLength(); i++) { // iterates over snake array to make sure the apple doesn't land on the snake
-      if ((apple.x === snakeArray[i].x) && (apple.y === snakeArray[i].y)) {
-        randomApple() // if the apple is on the snake, it randomizes the coordinates again
+    for (var x = 0; x < snakeArray.length; x++) {
+      for (var i = 1; i <= snakeLength(x); i++) { // iterates over snake array to make sure the apple doesn't land on the snake
+        if ((apple.x === snakeArray[x][i].x) && (apple.y === snakeArray[x][i].y)) {
+          randomApple() // if the apple is on the snake, it randomizes the coordinates again
+        }
       }
     }
   }
@@ -274,4 +341,5 @@ function runProgram() {
     apple.x = Math.ceil((Math.random() * apple.width)) * $board.columns - 2
     apple.y = Math.ceil((Math.random() * apple.height)) * $board.rows - 2
   }
+  
 }  
