84a867fc707d08221a1f20a6e13e2fdc607f12be
al-shep <alecsheppard22@gmail.com>
2021-06-21 15:10:35 -0500

    image filtering complete

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     const $display = $('#display');
 
     // TODO: Call your apply function(s) here
-
+    filter()
 
 
 
@@ -17,12 +17,67 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 // TODO 1 & 3: Create the applyFilter function here
+function applyFilter(filterFunction) {
 
+    for (var x = 0; x < image.length; x++) {
+        for (var i = 0; i < image[x].length; i++) {
+            var rgbString = image[x][i]
+            var rgbNumbers = rgbStringToArray(rgbString)
+            filterFunction(rgbNumbers)
+            rgbString = rgbArrayToString(rgbNumbers)
+            image[x][i] = rgbString
+        }
+    }
+
+}
 
 // TODO 5: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+    const initialBackground = image[0][0]
+    let white = "rgb(255, 255, 255)"
+    for (var x = 0; x < image.length; x++) {
+        for (var i = 0; i < image[x].length; i++) {
+            var rgbString = image[x][i]
+            if (rgbString === initialBackground) {
+                image[x][i] = white
+            }
+            else {
+                var rgbNumbers = rgbStringToArray(rgbString)
+                filterFunction(rgbNumbers)
+                rgbString = rgbArrayToString(rgbNumbers)
+                image[x][i] = rgbString
+            }
+        }
+    }
 
+}
 
-// TODO 2 & 4: Create filter functions
+// TODO 2 & 4: Create filter functions\
 
+let filterArr = []
+
+filterArr.push( 
+    function reddify(arr) {
+        arr[RED] = 255
+    }
+ )
+
+filterArr.push(
+    function decreaseBlue(arr) {
+        arr[BLUE] = Math.max( (arr[BLUE] - 30), 0 )
+    }
+)
+
+filterArr.push(
+    function increaseGreenByBlue(arr) {
+        arr[GREEN] = Math.min( (arr[GREEN] + arr[BLUE]), 255 )
+    }
+)
+
+function filter() {
+    for (var i = 0; i < filterArr.length; i++) {
+        applyFilterNoBackground(filterArr[i])
+    }
+}
 
 // CHALLENGE code goes below here
