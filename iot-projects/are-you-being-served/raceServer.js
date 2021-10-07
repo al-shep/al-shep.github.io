8aa6bee258c0100517767d9f10cf69a8677b4a06
al-shep <alecsheppard22@gmail.com>
2021-10-07 18:29:58 -0500

    saving are you being served

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -9,17 +9,30 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date()
+    let startTime = d.getTime()
 
     // TODO 12: Make the whole thing parallel
     async.series( 
         // TODO 9: Supply an array of functions
         [
-            
+            function(callback) {wrapper(callback)},
+            function(callback) {wrapper(callback)},
+            function(callback) {wrapper(callback)},
+            function(callback) {wrapper(callback)}
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
+            res.write('Results:\n')
+            let victoryOrder = sortTogether(racers, results)
 
+            for (var x = 0; x < victoryOrder.length; x++) {
+                res.write(victoryOrder[x] + '\n')
+            }
+
+            let d = new Date()
+            let endTime = d.getTime()
+            res.end((endTime - startTime).toString())
         }
     );
     
@@ -27,7 +40,10 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(function() {
+        let d = new Date()
+        callback(null, d.getTime)
+    }, Math.random()*1000)
 }
 
 function sortTogether(names, times) {
--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,28 @@
 // requestServer.js file
+
+
+// grab URL
+let args = process.argv.slice(2)
+
+//import http and request, setting port
+const http = require('http')
+const request = require('request')
+const port = 8686
+
+// server creation
+http.createServer(function(req, res) {
+    let url = args[0] ? args[0] : "<a default url>";
+    request(url, function(error, response, body) {
+        if (!body || !response || (error === null && response.statusCode !== 200)) {
+            res.end('bad URL\n')
+        } else if (response.statusCode === 200 && !error === true) {
+            res.writeHead(200, 'text/html')
+            res.write(body)
+            res.end()
+        } else {
+            res.writeHead(response.statusCode, 'text/plain')
+            res.write(error.toString())
+            res.end()
+        }
+    })
+}).listen(port)
