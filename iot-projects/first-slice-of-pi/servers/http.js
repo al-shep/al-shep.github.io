e31935c89aa48c0b88dbd2fe68429e27701ec67b
al-shep <alecsheppard22@gmail.com>
2021-11-09 17:52:02 -0600

    saving slice of pi

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,13 @@
 const express = require('express'),
 	cors = require('cors');
-	
+
+let app = express()
+app.use(cors())
+app.get('/', function(req, res){
+	res.send("You've hit the root")
+}
+app.get('/pi', function(req, res){
+	res.sent("Entering gateway now...")
+}
 
 module.exports = app;
