e69c299cd154f6b66cd61b1fa1e37667e0561d8b
al-shep <alecsheppard22@gmail.com>
2021-11-11 18:16:02 -0600

    updating fsop

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,5 @@
-let sensorRoutes = require('./../routes/sensors')
-let actuatorRoutes = require('./../routes/actuators')
+const sensorRoutes = require('./../routes/sensors')
+const actuatorRoutes = require('./../routes/actuators')
 
 const express = require('express'),
 	cors = require('cors');
@@ -7,7 +7,7 @@ const express = require('express'),
 let app = express()
 app.use(cors())
 app.use('/pi/sensors', sensorRoutes)
-app.use('/pi/sensors', actuatorRoutes)
+app.use('/pi/actuators', actuatorRoutes)
 
 app.get('/', function(req, res){
 	res.send("You've hit the root")
