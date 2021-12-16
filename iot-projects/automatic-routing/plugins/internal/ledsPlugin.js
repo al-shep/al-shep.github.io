c53be59328f46742601a33ff3d9475addec5ca68
al-shep <alecsheppard22@gmail.com>
2021-12-16 17:35:13 -0600

    slightly different solution

--- iot-projects/automatic-routing/plugins/internal/ledsPlugin.js
+++ iot-projects/automatic-routing/plugins/internal/ledsPlugin.js
@@ -1,6 +1,6 @@
 const resources = require('./../../resources/model');
 
-let actuator1, actuator2;
+let actuator
 let model = resources.pi.actuators.leds;
 let pluginName = resources.pi.actuators.leds[1].name + ", " + resources.pi.actuators.leds[2].name;
 
@@ -13,23 +13,25 @@ exports.start = function (params) {
 
 function connectHardware(){
 	const Gpio = require('onoff').Gpio
-	actuator1 = new Gpio(model[1].gpio, 'out')
-	actuator2 = new Gpio(model[2].gpio, 'out')
+	actuator = {
+		one : new Gpio(model[1].gpio, 'out'),
+		two : new Gpio(model[2].gpio, 'out')
+	}
 }
 
 exports.stop = function() {
-	actuator1.write(0)
-	actuator2.write(0)
-	actuator1.unexport()
-	actuator2.unexport()
+	actuator.one.write(0)
+	actuator.two.write(0)
+	actuator.one.unexport()
+	actuator.two.unexport()
 }
 
 exports.switchOnOff = {
 	1: function (value) {
-		Switch(value, 1)
+		Switch(value, one)
 	},
 	2: function (value, value) {
-		Switch(value, 2)
+		Switch(value, two)
 	}
 }
 
