d9b24cfa525bea75382db7eb37d6e5d39b76121a
al-shep <alecsheppard22@gmail.com>
2021-12-16 18:38:10 -0600

    saving code

--- iot-projects/automatic-routing/routes/actuators.js
+++ iot-projects/automatic-routing/routes/actuators.js
@@ -1,21 +1,9 @@
-const ledsPlugin = require('./../plugins/internal/ledsPlugin')
-
 const express = require('express'),
 	router = express.Router(),
-	resources = require('./../resources/model');
+	resources = require('./../resources/model'),
+	ledsPlugin = require('./../plugins/internal/ledsPlugin')
 
-router.route('/').get(function(req, res, next){
-	req.result = resources.pi.actuators
-	next()
-})
-router.route('/leds').get(function(req, res, next){
-	req.result = resources.pi.actuators.leds
-	next()
-})
-router.route('/leds/:id').get(function(req, res, next){
-	req.result = resources.pi.actuators.leds[req.params.id]
-	next()
-}).put(function(req, res, next){
+router.route('/leds/:id').put(function(req, res, next){
 	let led = resources.pi.actuators.leds
 	led.value = req.body.value
 	req.result = led
--- iot-projects/automatic-routing/servers/http.js
+++ iot-projects/automatic-routing/servers/http.js
@@ -1,6 +1,8 @@
-const sensorRoutes = require('./../routes/sensors');
-const actuatorRoutes = require('./../routes/actuators');
-const converter = require('./../middleware/converter')
+//const sensorRoutes = require('./../routes/sensors');
+const actuatorRoutes = require('./../routes/actuators'),
+	converter = require('./../middleware/converter'),
+	createRouter = require('./../routes/automate'),
+	resources = require('./../resources/model'),
 
 const bodyParser = require('body-parser');
 
@@ -10,16 +12,19 @@ const express = require('express'),
 let app = express();
 app.use(bodyParser.json())
 app.use(cors());
-app.use('/pi/sensors', sensorRoutes);
+//app.use('/pi/sensors', sensorRoutes);
 app.use('/pi/actuators', actuatorRoutes);
 
+/*
 app.get('/', function(req, res){
 	res.send("You've hit the root");
 });
 app.get('/pi', function(req, res){
 	res.send("Entering gateway now...");
 });
+*/
 
+app.use('/', createRouter(resources))
 app.use(converter())
 
 module.exports = app;
